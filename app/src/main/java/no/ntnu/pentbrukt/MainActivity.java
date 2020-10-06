fcee7a79ba5c9a0a0730ad325976a34366d9346b
Kjetil <kjetilhammerseth@gmail.com>
2020-10-06 11:19:29 +0200

    Checkpoint, toolbar caused fuckup.

--- app/src/main/java/no/ntnu/pentbrukt/MainActivity.java
+++ app/src/main/java/no/ntnu/pentbrukt/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
     }
 }
